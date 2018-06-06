31ebfcfac61d063a2d12464b0c86427b2accfc16
Nicholas Lepage <nic.lep@hotmail.com>
2018-06-06 16:30:57 +0000

    Did first exercise!

--- countLetters.js
+++ countLetters.js
@@ -1,3 +1,16 @@
-function countLetters() {
+var result = {};
 
+function countLetters(str) {
+  str = str.split(" ").join("").toLowerCase();
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] in result) {
+      result[str[i]] += 1;
+    } else {
+      result[str[i]] = 1;
+    }
+  }
+  return result;
 }
+
+console.log(countLetters("lighthouse in the house"));
+
